95ad9cf7a99516b18ee33cbcc4ccbf51a83cf5b2
RoyaKhurram <roya.khurram@student.csd509j.net>
2025-12-08 15:17:29 +0000

    Finished code

--- CommunityProject.java
+++ CommunityProject.java
@@ -14,15 +14,64 @@ public class CommunityProject {
     }
 
     // 3. Write a print() method that uses System.out.println to print out all the instance variables.
+    public void print()
+    {
+        System.out.println(eventName + " " + day + " " + month);
+    }
 
     // 4. Create accessor (get) methods for each of the instance variables.
+    public String getEventName()
+    {
+        return eventName;
+    }
+    public int getDay()
+    {
+        return day;
+    }
+    public int getMonth()
+    {
+        return month;
+    }
+    
 
     // 5. Create mutator (set) methods for each of the instance variables.
+    public void setEventName(String eventName)
+    {
+        this.eventName = eventName;
+    }
+    public void setDay(int day)
+    {
+        this.day = day;
+    }
+    public void setMonth(int month)
+    {
+        this.month = month;
+    }
 
     // 6. Create a toString() method that returns all the information in the instance variables.
+    public String toString()
+    {
+        return eventName + " " + day  + " "+ month;
+    }
+    
 
     // 7. Write an additional method for your class that takes a parameter. For example, there could be a print method with arguments that indicate how you want to print out the information, e.g. print(format) could print the data according to an argument that is "plain" or "table" where the data is printed in a table drawn with dashes (-) and lines (|).
+    public void infoFormat(String format)
+    {
+        String info;
+        if (format == "One Line" || format == "one line")
+        {
+            info = eventName + " is scheduled for " + day + "/" + month + ".";
+            System.out.println(info);
+        }
+        else if (format == "Detailed" || format == "detailed")
+        {
+            info = "Event Name: " + eventName + "\nDay: " + day + "\nMonth: " + month;
+            System.out.println(info);
+        }
+        
+    }
+}
 
     // 8. Write a main method that constructs at least 2 objects of your class using the constructor and then calls all of the methods that you created above to test them.
-
-}
+    
--- Main.java
+++ Main.java
@@ -2,7 +2,18 @@ class Main {
 
   public static void main(String[] args) {
     // Construct 2 objects of your class using the constructor with different values
+    CommunityProject e1 = new CommunityProject("Charity Run", 15, 6);
+    CommunityProject e2 = new CommunityProject("Food Drive", 22, 11);
 
     // call all of the objects methods to test them
+    e1.print();
+    e2.print();
+    System.out.println(e1.getEventName());
+    System.out.println(e2.getDay());
+    e1.setMonth(7);
+    System.out.println(e1.toString());
+    e2.infoFormat("Detailed");
+    
+
   }
 }
