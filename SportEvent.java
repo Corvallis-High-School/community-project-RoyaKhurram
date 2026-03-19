45d2886aab68a320de9002fed913fe817972b867
RoyaKhurram <roya.khurram@student.csd509j.net>
2026-03-19 19:12:00 +0000

    SportEvent.java added

---
+++ SportEvent.java
@@ -0,0 +1,39 @@
+public class SportEvent extends CommunityProject
+{
+    private String sport;
+    private String teamA;
+    private String teamB;
+    private int maxTickets;
+    private int score;
+    public SportEvent(String eventName, int day, int month, int year, String time, String location, String sport, String teamA, String teamB, int score)
+    {
+        super(eventName, day, month, year, time, location);
+        this.sport = sport;
+        this.teamA = teamA;
+        this.teamB = teamB;
+        this.score = score;
+    
+    }
+    public SportEvent()
+    {}
+    public String toString()
+    {
+        return super.toString() + " game of: " + sport + ", " + teamA + " against " + teamB + " Current score of "  + teamA + " is: " + score;
+
+    }
+    public void addPoint()
+    {
+        score++;
+    }
+    public void removePoint()
+    {
+        score--;
+    }
+    public int getScore()
+    {
+        return score;
+    }
+    
+
+
+}
