60ea264e3702499ce66340199b8475264f98c9d5
RoyaKhurram <roya.khurram@student.csd509j.net>
2026-03-19 21:57:46 +0000

    added main method to main class CommunityProject

--- CommunityProject.java
+++ CommunityProject.java
@@ -50,6 +50,11 @@ public class CommunityProject {
     {
         return location;
     }
+    public int getYear()
+    {
+        return year;
+    }
+
 
     // 5. Create mutator (set) methods for each of the instance variables.
     public void setEventName(String eventName)
@@ -71,6 +76,10 @@ public class CommunityProject {
     public void setLocation(String location)
     {
         this.location= location;
+    }
+    public void setYear(int year)
+    {
+        this.year = year;
     }
 
 
@@ -97,5 +106,22 @@ public class CommunityProject {
         }
         
     }
+    public static void main(String[] args) {
+    // Construct 2 objects of your class using the constructor with different values
+    CommunityProject e1 = new FundraiserEvent("Charity Run", 15, 6, 2026, "3:00 pm", "Avery Park", 9000.0, " raise awareness for cancer ");
+    CommunityProject e2 = new SportEvent("Home Game", 22, 11, 2026, "12:00 pm", "Corvallis High School", "Basektball", "Spartians", "Raiders", 20);
+
+
+    // call all of the objects methods to test them
+    e1.print();
+    e2.print();
+    System.out.println(e1.getEventName());
+    System.out.println(e2.getDay());
+    e1.setMonth(7);
+    System.out.println(e1.toString());
+    e2.infoFormat("Detailed");
+    }
 }
+
+
     
